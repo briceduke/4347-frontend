2494226d99b73544aa8ea56f7c130ee0054822cb
Brice Duke <bricewduke@gmail.com>
2024-11-04 18:50:03 -0600

    feat(global): initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+	return (
+		<div className="items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+			<h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+				4347 Project Frontend
+			</h1>
+
+			<p>Coming soon</p>
+		</div>
+	);
+}
